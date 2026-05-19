fe1e9d2b863bc8c79e067afe59e39c2dea336734
nachool <nachma.olivia1394@gmail.com>
2026-05-19 14:26:45 +0700

    memanggil method userrepository dan passwordencoder

--- src/main/java/com/deploy11/praktikum11/service/AuthService.java
+++ src/main/java/com/deploy11/praktikum11/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.deploy11.praktikum11.service;
 
+import com.deploy11.praktikum11.repository.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,5 +8,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 }
