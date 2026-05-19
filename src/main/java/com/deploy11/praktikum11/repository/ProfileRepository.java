dc7134dda65fa0cc77d5542f42a8b7e35ae8abe5
nachool <nachma.olivia1394@gmail.com>
2026-05-19 14:19:39 +0700

    ProfileRepository

--- src/main/java/com/deploy11/praktikum11/repository/ProfileRepository.java
+++ src/main/java/com/deploy11/praktikum11/repository/ProfileRepository.java
@@ -1,4 +1,9 @@
 package com.deploy11.praktikum11.repository;
 
-public class ProfileRepository {
+import com.deploy11.praktikum11.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, String> {
 }
