fb3b2491e3013a9d4e72cc0f7a171e592d1fc09c
nachool <nachma.olivia1394@gmail.com>
2026-05-19 14:25:42 +0700

    import untuk authservice

--- src/main/java/com/deploy11/praktikum11/service/AuthService.java
+++ src/main/java/com/deploy11/praktikum11/service/AuthService.java
@@ -1,4 +1,11 @@
 package com.deploy11.praktikum11.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
 public class AuthService {
 }
