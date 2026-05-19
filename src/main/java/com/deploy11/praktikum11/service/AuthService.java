4d1dd233e101c2c501ec309d1b65d0e2a8d201d8
nachool <nachma.olivia1394@gmail.com>
2026-05-19 14:27:36 +0700

    method regist

--- src/main/java/com/deploy11/praktikum11/service/AuthService.java
+++ src/main/java/com/deploy11/praktikum11/service/AuthService.java
@@ -1,5 +1,8 @@
 package com.deploy11.praktikum11.service;
 
+import com.deploy11.praktikum11.model.Profile;
+import com.deploy11.praktikum11.model.User;
+import com.deploy11.praktikum11.model.dto.RegisterRequest;
 import com.deploy11.praktikum11.repository.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +20,26 @@ public class AuthService {
 
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    public void register(RegisterRequest request) {
+
+        User user = User.builder()
+                .id(UUID.randomUUID().toString())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+        Profile profile = Profile.builder()
+                .id(UUID.randomUUID().toString())
+                .nama(request.getNama())
+                .alamat(request.getAlamat())
+                .user(user)
+                .build();
+
+        user.setProfile(profile);
+
+        userRepository.save(user);
     }
 
 }
