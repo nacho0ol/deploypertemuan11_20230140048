acb7d57699c8d40921d9bcb435ea3e5be3c759d1
nachool <nachma.olivia1394@gmail.com>
2026-05-19 14:15:41 +0700

    import

--- src/main/java/com/deploy11/praktikum11/model/User.java
+++ src/main/java/com/deploy11/praktikum11/model/User.java
@@ -1,4 +1,18 @@
 package com.deploy11.praktikum11.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
 }
