f4e994afef1c63525b20f5e446eb8ccf0866b2b8
nachool <nachma.olivia1394@gmail.com>
2026-05-19 14:10:10 +0700

    import entity, data, builder, table dengan nama profile, constructor

--- src/main/java/com/deploy11/praktikum11/model/Profile.java
+++ src/main/java/com/deploy11/praktikum11/model/Profile.java
@@ -1,4 +1,17 @@
 package com.deploy11.praktikum11.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profile {
 }
