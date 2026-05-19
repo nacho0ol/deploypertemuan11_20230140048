12113185aa37a6ad3a6bb8decdfd6e302be80fe2
nachool <nachma.olivia1394@gmail.com>
2026-05-19 14:20:36 +0700

    UserRepository

--- src/main/java/com/deploy11/praktikum11/repository/UserRepository.java
+++ src/main/java/com/deploy11/praktikum11/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.deploy11.praktikum11.repository;
 
-public interface UserRepository {
+import com.deploy11.praktikum11.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
 }
