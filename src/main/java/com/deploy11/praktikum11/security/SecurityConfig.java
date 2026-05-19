8e590c4bc2e5dc00ccded7b44e29d6327d63ddde
nachool <nachma.olivia1394@gmail.com>
2026-05-19 14:22:07 +0700

    import security

--- src/main/java/com/deploy11/praktikum11/security/SecurityConfig.java
+++ src/main/java/com/deploy11/praktikum11/security/SecurityConfig.java
@@ -1,4 +1,19 @@
 package com.deploy11.praktikum11.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 }
